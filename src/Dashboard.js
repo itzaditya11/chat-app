bf72620119f8d42ace7dbbca900adbcbdeb2d598
aditya <imaditya1106@gmail.com>
2020-10-01 10:56:48 +0530

    inital commit

---
+++ src/Dashboard.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+import { CTX } from "./Store";
+
+const styles = (theme) => ({
+  root: {
+    ...theme.mixins.gutters(),
+    margin: "50px",
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  flex: {
+    display: "flex",
+    alignItems: "center",
+  },
+  topicsWindow: {
+    width: "30%",
+    height: "300px",
+    borderRight: "1px solid grey",
+  },
+  chatWindow: {
+    width: "70%",
+    height: "300px",
+    padding: "20px",
+  },
+  chatBox: {
+    width: "85%",
+  },
+  button: {
+    width: "15%",
+  },
+});
+
+const Dashboard = (props) => {
+  const { classes } = props;
+
+  // CTX store
+  const { allChats, sendChatAction, user } = React.useContext(CTX);
+
+  const topics = Object.keys(allChats);
+
+  // local state
+  const [textValue, changeTextValue] = React.useState("");
+  const [activeTopic, setActiveTopic] = React.useState(topics[0]);
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="h4" component="h4">
+          Chat App
+        </Typography>
+        <Typography component="h5" variant="h5">
+          {activeTopic}
+        </Typography>
+        <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
+            <List>
+              {topics.map((topic) => (
+                <ListItem button key={topic}>
+                  <ListItemText
+                    primary={topic}
+                    onClick={() => setActiveTopic(topic)}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {allChats[activeTopic].map((chat, index) => (
+              <div className={classes.flex} key={index}>
+                <Chip label={chat.from} className={classes.chip} />
+                <Typography variant="subtitle1">{chat.msg}</Typography>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={classes.flex}>
+          <TextField
+            label="Send a message"
+            className={classes.chatBox}
+            value={textValue}
+            onChange={(evt) => changeTextValue(evt.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={() => {
+              sendChatAction({
+                msg: textValue,
+                from: user,
+                topic: activeTopic,
+              });
+              changeTextValue("");
+            }}
+          >
+            Send
+          </Button>
+        </div>
+      </Paper>
+    </div>
+  );
+};
+
+export default withStyles(styles)(Dashboard);
---
+++ src/Store.js
@@ -0,0 +1,66 @@
+import React from "react";
+import io from "socket.io-client";
+
+export const CTX = React.createContext();
+
+const initialState = {
+  general: [
+    { from: "Rotimi", msg: "Hello" },
+    { from: "John", msg: "Hello" },
+    { from: "Titi", msg: "Hello" },
+  ],
+  random: [
+    { from: "Jane", msg: "Hello" },
+    { from: "Tom", msg: "Hello" },
+    { from: "Jennifer", msg: "Hello" },
+  ],
+};
+
+function reducer(state, action) {
+  const { from, msg, topic } = action.payload;
+  console.log(action, ">>>");
+  switch (action.type) {
+    case "RECEIVE_MESSAGE":
+      return {
+        ...state,
+        [topic]: [
+          ...state[topic],
+          {
+            from,
+            msg,
+          },
+        ],
+      };
+    default:
+      return state;
+  }
+}
+
+let socket;
+
+// this should get called after the Store
+// has mounted
+const sendChatAction = (value) => {
+  socket.emit("chat message", value);
+};
+
+const Store = (props) => {
+  const user = `Rotimi${Math.random(100).toFixed(2)}`;
+
+  const [allChats, dispatch] = React.useReducer(reducer, initialState);
+  if (!socket) {
+    socket = io(":3001");
+
+    socket.on("chat message", (msg) => {
+      dispatch({ type: "RECEIVE_MESSAGE", payload: msg });
+    });
+  }
+
+  return (
+    <CTX.Provider value={{ allChats, sendChatAction, user }}>
+      {props.children}
+    </CTX.Provider>
+  );
+};
+
+export default Store;
